9d0b0ac8cb97ab6ffea14fc5a911c04f06b030ce
Emori03 <emorijan@gmail.com>
2024-11-27 01:11:50 -0600

    Cambios para arreglar errores del codigo

--- app/src/main/java/com/example/movietrailerstv/domain/Category.kt
+++
@@ -1,8 +0,0 @@
-package com.example.movietrailerstv.domain
-
-enum class Category(val id: String) {
-    POPULAR("popularity.desc"),
-    NEW("release_date.desc"),
-    VOTES("vote_average.desc"),
-    REVENUE("revenue.desc")
-}
